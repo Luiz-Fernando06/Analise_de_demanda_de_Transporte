d24b1c6b5404695295d285e6558fe82ccf0e5e01
Luiz Fernando <e.luizfernando2006@gmail.com>
2025-10-19 12:44:38 -0300

    Update AnaliseDemanda.py

--- AnaliseDemanda.py
+++ AnaliseDemanda.py
@@ -35,3 +35,25 @@ print(f"Horario de maior demanda: {horario_maior_demanda.idxmax()}h")
 semana_maior_demanda = df['dia_semana'].value_counts().sort_index()
 print('\n',semana_maior_demanda)
 print(f"Dia da semana de maior demanda: {semana_maior_demanda.idxmax()}")
+
+
+#heatmap textual(para o grafico):
+
+# Definindo a ordem correta das semanas
+ordem_dias = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+# Transforme a coluna dia_semana em categoria com a ordem desejada
+df['dia_semana'] = pd.Categorical(df['dia_semana'], categories=ordem_dias, ordered=True)
+'''
+pd.Categorical(..., ordered=True) permite que o pandas saiba qual é a ordem lógica, e não apenas alfabética.
+'''
+# Dia da semana x hora
+heatmap = df.groupby(['dia_semana', 'hora']).size().unstack(fill_value=0)
+print('\n',heatmap)
+'''
+Linhas → dias da semana
+
+Colunas → hora do dia (0–23)
+
+Valores → quantidade de corridas (demanda)
+'''
+
